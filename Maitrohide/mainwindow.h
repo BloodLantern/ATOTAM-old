a62a708885600ef6b93a429b728c2454e12488d6
cc-hunter-boy <celiangdb@gmail.com>
2022-03-13 12:50:25 +0100

    zt

--- Maitrohide/mainwindow.h
+++ Maitrohide/mainwindow.h
@@ -14,7 +14,7 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    MainWindow(QApplication *app, QWidget *parent = nullptr);
     ~MainWindow();
     virtual void paintEvent(QPaintEvent*);
 
