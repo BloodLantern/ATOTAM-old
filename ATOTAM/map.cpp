d49d6609eabdd40a92963804cfa03b635fb30342
BloodLantern <maxime.gri.berlioz@gmail.com>
2022-06-06 11:43:00 +0200

    Improved map Json system
    In a map Json, we can now set a 'times' value and a 'vertical' boolean. 'times' is the number of times this entity will be placed in a row. 'vertical' is whether the copies will be placed vertically or horizontally.

--- ATOTAM/map.cpp
+++ ATOTAM/map.cpp
@@ -33,42 +33,53 @@ std::vector<Entity *> Map::loadRoom(int id)
         for (const std::pair<std::string, nlohmann::json> name : entity.second.get<nlohmann::json::object_t>())
             // For each entity json node
             for (nlohmann::json obj : name.second) {
-                Entity* e = nullptr;
+                // Repeat 'obj["times"]' times
+                for (unsigned int i = 0; i < (obj["times"].is_null() ? 1 : static_cast<unsigned int>(obj["times"])); i++) {
+                    Entity* e = nullptr;
 
-                // Specific Entities fields and initialization
-                if (entity.first == "Terrain") {
-                    Terrain *t = new Terrain(static_cast<int>(roomJson["position"][0]) + static_cast<int>(obj["x"]),
-                            static_cast<int>(roomJson["position"][1]) + static_cast<int>(obj["y"]), name.first);
-                    e = t;
-                } else if (entity.first == "Door") {
-                    Door *d = new Door(static_cast<int>(roomJson["position"][0]) + static_cast<int>(obj["x"]),
-                            static_cast<int>(roomJson["position"][1]) + static_cast<int>(obj["y"]), name.first);
-                    e = d;
-                    d->setEndingRoom(obj["to"]);
-                } else if (entity.first == "NPC") {
-                    NPC *npc = new NPC(static_cast<int>(roomJson["position"][0]) + static_cast<int>(obj["x"]),
-                            static_cast<int>(roomJson["position"][1]) + static_cast<int>(obj["y"]), obj["facing"], name.first);
-                    e = npc;
-                } else if (entity.first == "Monster") {
-                    Monster *m = new Monster(static_cast<int>(roomJson["position"][0]) + static_cast<int>(obj["x"]),
-                            static_cast<int>(roomJson["position"][1]) + static_cast<int>(obj["y"]), obj["facing"], name.first);
-                    e = m;
+                    int x = static_cast<int>(roomJson["position"][0]) + static_cast<int>(obj["x"]);
+                    int y = static_cast<int>(roomJson["position"][1]) + static_cast<int>(obj["y"]);
+                    std::string n = name.first; // Name
+
+                    if (!obj["times"].is_null())
+                        if (!obj["vertical"].is_null()) { // Should always be true
+                            if (obj["vertical"])
+                                y += i * static_cast<int>(Entity::values["names"][n]["height"]);
+                            else
+                                x += i * static_cast<int>(Entity::values["names"][n]["width"]);
+                        }
+
+                    // Specific Entities fields and initialization
+                    if (entity.first == "Terrain") {
+                        Terrain *t = new Terrain(x, y, n);
+                        e = t;
+                    } else if (entity.first == "Door") {
+                        Door *d = new Door(x, y, n);
+                        e = d;
+                        d->setEndingRoom(obj["to"]);
+                    } else if (entity.first == "NPC") {
+                        NPC *npc = new NPC(x, y, obj["facing"], n);
+                        e = npc;
+                    } else if (entity.first == "Monster") {
+                        Monster *m = new Monster(x, y, obj["facing"], n);
+                        e = m;
+                    }
+
+                    // Make sure not to use a null Entity pointer
+                    if (e == nullptr)
+                        continue;
+
+                    // Shared entity fields
+                    if (obj["state"].is_null())
+                        e->setState("None");
+                    else
+                        e->setState(obj["state"]);
+
+                    e->setCurrentAnimation(e->updateAnimation(e->getState()));
+                    e->setFrame(0);
+                    e->updateTexture();
+                    entities.push_back(e);
                 }
-
-                // Make sure not to use a null Entity pointer
-                if (e == nullptr)
-                    continue;
-
-                // Shared entity fields
-                if (obj["state"].is_null())
-                    e->setState("None");
-                else
-                    e->setState(obj["state"]);
-
-                e->setCurrentAnimation(e->updateAnimation(e->getState()));
-                e->setFrame(0);
-                e->updateTexture();
-                entities.push_back(e);
             }
     return entities;
 }
